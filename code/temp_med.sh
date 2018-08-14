7e008a04e012e689d985485881688bc2dff7f6e1
John Chen <john.person1@gmail.com>
2018-08-14 11:15:57 -0400

    going forward, should use the SPECIFIC python only

--- code/temp_med.sh
+++ code/temp_med.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-python2 style_transfer.py --train ../data/med/medical.train --dev ../data/med/medical.dev --output ../tmp/medical.dev --vocab ../tmp/medical.vocab --model ../tmp/model
+# source activate CAE
+# which python2
+# specify the RIGHT python to use! (according to how you might normally run it)
+# conda env list
+# /h/johnchen/anaconda3/envs
+~/anaconda3/envs/CAE/bin/python2 style_transfer.py --train ../data/med/final_medical.train --dev ../data/med/final_medical.dev --output ../tmp/final_medical.dev --vocab ../tmp/final_medical.vocab --model ../tmp/final_medical_model --batch_size 64
 
 
 
